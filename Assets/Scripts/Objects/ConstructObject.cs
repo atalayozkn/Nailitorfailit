1774f2349fd7ea1a1806ce980dd10a752f11e80e
Tarık Hatipoğlu <tarikhatipoglu21@gmail.com>
2026-06-07 21:01:55 +0300

    07.06.2026 - 21:00
    ConstructObject multiplayer olan kodun içine interactionMesh eklendi böylece duvar veya parke inşa olduktan sonra tekrar interact etmeye gerek kalmayacak
    
    Bu da diğer boş inşaatların daha kolay interact olmasını sağlayacak

--- Assets/Scripts/Objects/ConstructObject.cs
+++ Assets/Scripts/Objects/ConstructObject.cs
@@ -12,6 +12,7 @@ namespace ItemScript
         [SerializeField] private ConstructProfile profile;
 
         [Header("Visuals (Assign in Inspector)")]
+        [SerializeField] private GameObject interactionMesh;
         [SerializeField] private GameObject ghostMesh;
         [SerializeField] private GameObject builtMesh;
 
@@ -37,7 +38,10 @@ namespace ItemScript
         private void UpdateVisuals(bool built)
         {
             if (ghostMesh) ghostMesh.SetActive(!built);
-            if (builtMesh) builtMesh.SetActive(built);
+
+            if (builtMesh)
+                interactionMesh.SetActive(!built);
+                builtMesh.SetActive(built);
         }
 
         public void Interact()
